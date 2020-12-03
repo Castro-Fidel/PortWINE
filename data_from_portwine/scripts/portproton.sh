999b4592be8f1b30d2b052026486af9b2d29de14
castro-fidel <tergoevm@gmail.com>
2020-12-03 22:41:55 +0300

    Added silent installation

--- data_from_portwine/scripts/portproton.sh
+++ data_from_portwine/scripts/portproton.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 # Author: PortWINE-Linux.ru
 . "$(dirname $(readlink -f "$0"))/runlib"
+START_PORTWINE
 if [ -f "$1" ]; then
     export portwine_exe="$(readlink -f "$1")"
     export PATH_TO_GAME="$( cd "$( dirname "$1" )" >/dev/null 2>&1 && pwd )"
-    START_PORTWINE
     if [ ! -z ${optirun_on} ]; then
         "${PW_RUNTIME}" $PW_TERM ${optirun_on} "${port_on_run}" "run" "$portwine_exe"
     else
         "${PW_RUNTIME}" $PW_TERM "${port_on_run}" "run" "$portwine_exe"
     fi
 else
-    START_PORTWINE
-    sh "${PORT_SCRIPTS_PATH}/winefile"
+    if [ ! -z ${optirun_on} ]
+    then
+        "${PW_RUNTIME}" $PW_TERM ${optirun_on} "${port_on_run}" "run" "explorer" 
+    else
+        "${PW_RUNTIME}" $PW_TERM "${port_on_run}" "run" "explorer" 
+    fi
 fi
 STOP_PORTWINE
