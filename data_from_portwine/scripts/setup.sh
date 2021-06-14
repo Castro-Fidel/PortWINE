295d028ba711aaa352cc6f77ed9c095d25ff818c
castro-fidel <tergoevm@gmail.com>
2021-06-14 22:22:00 +0300

    ###Scripts version 1104###

--- data_from_portwine/scripts/setup.sh
+++ data_from_portwine/scripts/setup.sh
@@ -8,7 +8,7 @@ try_remove_file "${PORT_WINE_TMP_PATH}/init_run_suc"
 try_remove_file "${PORT_WINE_PATH}/data/port_on"
 try_remove_file "${PORT_WINE_PATH}/settings.desktop"
 try_remove_file "${PORT_WINE_PATH}/debug.desktop"
-try_remove_file "${PORT_WINE_PATH}/restart.desktop"
+try_remove_file "${PORT_WINE_PATH}/reset.desktop"
 
 try_remove_dir "${PORT_WINE_PATH}/data/pfx/dosdevices"
 try_remove_dir "${PORT_WINE_PATH}/Settings"
@@ -16,7 +16,7 @@ try_remove_dir "${PORT_SCRIPTS_PATH}/vars"
 
 create_new_dir "/home/${USER}/.local/share/applications"
 
-name_desktop="PortProton" 
+name_desktop="PortProton"
 echo "[Desktop Entry]"	 					  > "${PORT_WINE_PATH}/${name_desktop}.desktop"
 echo "Name=${name_desktop}" 				 >> "${PORT_WINE_PATH}/${name_desktop}.desktop"
 echo "Exec=env "${PORT_SCRIPTS_PATH}/start.sh %F""	 >> "${PORT_WINE_PATH}/${name_desktop}.desktop"
@@ -33,7 +33,7 @@ cp -f "${PORT_WINE_PATH}/${name_desktop}.desktop" /home/${USER}/.local/share/app
 update-desktop-database -q "${HOME}/.local/share/applications"
 xdg-mime default PortProton.desktop "application/x-ms-dos-executable;application/x-wine-extension-msp;application/x-msi;application/x-msdos-program"
 
-name_desktop="readme" 
+name_desktop="readme"
 echo "[Desktop Entry]"					 > "${PORT_WINE_PATH}/${name_desktop}.desktop"
 echo "Name=${name_desktop}"				>> "${PORT_WINE_PATH}/${name_desktop}.desktop"
 echo "Version=1.0"					>> "${PORT_WINE_PATH}/${name_desktop}.desktop"
@@ -42,25 +42,11 @@ echo "Icon="${PORT_WINE_PATH}/data/img/readme.png""	>> "${PORT_WINE_PATH}/${name
 echo "URL=${urlg}" >> "${PORT_WINE_PATH}/${name_desktop}.desktop"
 chmod u+x "${PORT_WINE_PATH}/${name_desktop}.desktop"
 
-for name_desktop in "reset"
-do
-	echo ""[Desktop Entry]"
-	"Name=${name_desktop}"
-	"Exec=env "${PORT_SCRIPTS_PATH}/${name_desktop}""
-	"Type=Application"
-	"Categories=Game"
-	"StartupNotify=true"
-	"Path="${PORT_SCRIPTS_PATH}/""
-	"Icon="${PORT_WINE_PATH}/data/img/s.png""" > "${PORT_WINE_PATH}/${name_desktop}.desktop"
-	chmod u+x "${PORT_WINE_PATH}/${name_desktop}.desktop"
-done
-chmod u+x "${PORT_SCRIPTS_PATH}/"*
-
 if [ "${s_install}" = "1" ]; then
 	echo "Installation completed successfully."
 else
-	`zenity --info --title "${inst_set_top}" --text "${inst_succ}" --no-wrap ` > /dev/null 2>&1  
+	`zenity --info --title "${inst_set_top}" --text "${inst_succ}" --no-wrap ` > /dev/null 2>&1
 	xdg-open "http://portwine-linux.ru/portwine-faq/" > /dev/null 2>&1 & exit 0
-fi  
+fi
 
 unset INSTALLING_PORT
