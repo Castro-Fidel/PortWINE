ab837e46e019309882149c97af7233b4f5a36d0d
castro-fidel <tergoevm@gmail.com>
2022-01-15 17:43:37 +0300

    update post-install script 2

--- data_from_portwine/scripts/setup.sh
+++ data_from_portwine/scripts/setup.sh
@@ -35,10 +35,12 @@ if [ "${PW_SILENT_INSTALL}" = "1" ] ; then
 		unset INSTALLING_PORT
 		if [[ -f "${HOME}/.local/share/applications/PortProton.desktop" ]] ; then
 			export PW_OLD_PATH=`cat "${HOME}/.local/share/applications/PortProton.desktop" | grep -w 'Path=' | sed -E 's/Path=//' | sed -E 's%\/PortProton\/data\/scripts\/%%g' `
+			echo "PW_OLD_PATH=${PW_OLD_PATH}"
 			try_remove_file "${HOME}/.local/share/applications/PortProton.desktop"
 		fi
 		if [[ ! -z "${PW_OLD_PATH}" ]]	; then 
 			if [[ "${PW_OLD_PATH}"* == "${HOME}/PortWINE"* ]] & [[ -d "${HOME}/PortWINE" ]] ; then
+				echo "Old path = ${HOME}/PortWINE"
 				mv -f "${HOME}/PortWINE" "${XDG_DATA_HOME}"
 			elif [[ "${PW_OLD_PATH}"* == "${PW_OLD_PATH}/PortWINE"* ]] & [[ -d "${PW_OLD_PATH}/PortWINE" ]] ; then
 				ln -s "${PW_OLD_PATH}/PortWINE" "${XDG_DATA_HOME}/"
