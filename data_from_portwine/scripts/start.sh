f43f247be54b46b1f5225da92e998a781bfdcd1c
Mikhail Tergoev <fidel@altlinux.org>
2023-07-23 21:44:18 +0300

    Scripts version 2207

--- data_from_portwine/scripts/start.sh
+++ data_from_portwine/scripts/start.sh
@@ -446,7 +446,6 @@ pw_autoinstall_from_db () {
     export PW_MANGOHUD=0
     export ENABLE_VKBASALT=0
     export PW_USE_D3D_EXTRAS=1
-    export PW_LOG=1
     . "${PORT_SCRIPTS_PATH}/pw_autoinstall/${PW_YAD_SET}"
 }
 
