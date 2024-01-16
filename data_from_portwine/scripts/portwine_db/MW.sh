4a37ea80c13ea241336951ac2be5d376d7299155
valokardin <112694056+valokardin@users.noreply.github.com>
2024-01-16 10:07:38 +0200

    Update MW.sh

--- data_from_portwine/scripts/portwine_db/MW.sh
+++ data_from_portwine/scripts/portwine_db/MW.sh
@@ -5,11 +5,8 @@
 export PW_COMMENT_DB="Modern Warships"
 ################################################
 export PW_VULKAN_USE=2                       #dxvk, vkd3d or 0 for OpenGL
-export LAUNCH_PARAMETERS=""            # Additional launch options
-export PW_WINDOWS_VER=10                        # Set windows version 10, 7 or XP
-export WINEDLLOVERRIDES=""
 export PW_DLL_INSTALL="lucida"     # Install DLL in port prefix (used winetricks)
 export PW_USE_D3D_EXTRAS="1"
 export PW_USE_EAC_AND_BE="0"
 export PW_PREFIX_NAME="MW"
-export PW_USE_SHADER_CACHE="0"
+export PW_USE_SHADER_CACHE="1"
