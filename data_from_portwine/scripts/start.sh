2ef6b10ef88125fd722c64a9cb905e1b90de999b
Htylol <dixperm909@yandex.ru>
2024-08-08 21:30:07 +0500

    Fixed gettext

--- data_from_portwine/scripts/start.sh
+++ data_from_portwine/scripts/start.sh
@@ -63,8 +63,7 @@ export PORT_SCRIPTS_PATH PORT_WINE_PATH
 export TEXTDOMAIN="PortProton"
 export TEXTDOMAINDIR="${PORT_WINE_PATH}/data/locales"
 
-if [[ ! -d "$TEXTDOMAINDIR" ]] \
-&& ! command -v gettext &>/dev/null
+if ! command -v gettext &>/dev/null
 then
     gettext() { echo "$1"; }
 fi
