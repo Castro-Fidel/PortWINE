7369c200a3d5a1c4a932ee1b31330d68f00eb5c5
Sergey Palcheh <minergenon@altlinux.org>
2024-09-30 14:34:35 +0600

    Added game Lost Light

--- data_from_portwine/scripts/start.sh
+++ data_from_portwine/scripts/start.sh
@@ -753,6 +753,7 @@ else
     --field="   Blood and Soul"!"$PW_GUI_ICON_PATH/bloodandsoul.png"!"":"FBTN" '@bash -c "button_click --normal PW_BLOOD_AND_SOUL"' \
     --field="   Star Conflict"!"$PW_GUI_ICON_PATH/starconflict.png"!"":"FBTN" '@bash -c "button_click --normal PW_STAR_CONFLICT"' \
     --field="   GameXP"!"$PW_GUI_ICON_PATH/gamexp.png"!"":"FBTN" '@bash -c "button_click --normal PW_GAME_XP"' \
+    --field="   Lost Light"!"$PW_GUI_ICON_PATH/lostlight.png"!"":"FBTN" '@bash -c "button_click --normal PW_LOST_LIGHT"' \
     2>/dev/null &
 
     export START_FROM_PP_GUI="1"
