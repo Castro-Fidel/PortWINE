4e060f821558aae0d05b3a436ca1c1dd16aae0b2
Mikhail Tergoev <fidel@altlinux.org>
2024-11-09 15:08:55 +0300

    change recomend dpi and added 80 dpi

--- data_from_portwine/scripts/start.sh
+++ data_from_portwine/scripts/start.sh
@@ -245,10 +245,11 @@ if [[ ! $PW_GUI_START =~ (PANED|NOTEBOOK) ]] ; then
 fi
 
 # TODO:fixes_after_update
-if fixes_after_update "2371: Reset PW_WINE_DPI_VALUE in user.conf" ; then
+if fixes_after_update "2372: Reset PW_WINE_DPI_VALUE in user.conf" ; then
     sed -i '/export PW_WINE_DPI_VALUE=/d' "$USER_CONF"
     unset PW_WINE_DPI_VALUE
 fi
+
 # choose wine dpi default
 if [[ -z $PW_WINE_DPI_VALUE ]] ; then
     echo 'export PW_WINE_DPI_VALUE="recommended"' >> "$USER_CONF"
